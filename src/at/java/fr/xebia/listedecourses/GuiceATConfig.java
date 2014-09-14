f73d6941035d5007168b4c16fc129094a7954991
Pierre-Jean Vardanega <pierrejean.vardanega@gmail.com>
2014-09-14 23:13:56 +0200

    Add a first cucumber test making a search on google.fr

---
+++ src/at/java/fr/xebia/listedecourses/GuiceATConfig.java
@@ -0,0 +1,14 @@
+package fr.xebia.listedecourses;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import com.google.inject.Binder;
+import com.google.inject.Module;
+
+public class GuiceATConfig implements Module {
+
+    @Override
+    public void configure(Binder binder) {
+        binder.bind(WebDriver.class).to(FirefoxDriver.class);
+    }
+}
---
+++ src/at/java/fr/xebia/listedecourses/ListeDeCoursesAT.java
@@ -0,0 +1,11 @@
+package fr.xebia.listedecourses;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(format = "pretty", features = "src/at/resources")
+public class ListeDeCoursesAT {
+
+}
